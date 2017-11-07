9a9133303e23b024e7ee2889800334c3d155bd37
Sucaretto <28331534+Sucaretto@users.noreply.github.com>
2017-11-07 11:13:39 +0800

    更新内容: 1,添加了注释(给我自己看的); 2,删除了多余的代码; 3,添加了记录悬浮窗位置的功能.
    待解决问题:
    代码还是不够精简;
    执行某些指令时提示拒绝访问(如ifconfig);
    待补充...

--- src/com/Floating/ShellRun/Setting.java
+++ src/com/Floating/ShellRun/Setting.java
@@ -1,7 +1,5 @@
 package com.Floating.ShellRun;
 
-import com.floatingview.shell.R;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -21,14 +19,33 @@ public class Setting extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.setting);
-
+		// 初始化
 		edit = (EditText) findViewById(R.id.cmd_in);
 		box1 = (CheckBox) findViewById(R.id.root);
 		box2 = (CheckBox) findViewById(R.id.out);
 		box3 = (CheckBox) findViewById(R.id.move);
 		box4 = (CheckBox) findViewById(R.id.doubleclick);
 		text = (TextView) findViewById(R.id.setting_out);
-		loading();
+		loading();// 载入信息
+	}
+
+	private void loading() {// 载入信息
+		SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);// 指定读取的文件
+		String cmd = pref.getString("cmd", "");// 执行的命令
+		String root = "" + pref.getBoolean("root", false);// 是否需要root
+		String out = "" + pref.getBoolean("out", false);// 输出
+		String move = "" + pref.getBoolean("move", false);// 移动
+		String doubleclick = "" + pref.getBoolean("doubleclick", false);// 双击模式
+		text.setText("指令：" + cmd + "\n" + "Root：" + root + "\n" + "输出：" + out
+				+ "\n" + "移动模式" + move + "\n" + "双击模式" + doubleclick);// 输出设定内容
+		// 根据文件记录信息,设定状态
+		edit.setText(cmd);
+		box1.setChecked(pref.getBoolean("root", false));
+		box2.setChecked(pref.getBoolean("out", false));
+		box3.setChecked(pref.getBoolean("move", false));
+		mode = pref.getBoolean("move", false);
+		box4.setChecked(pref.getBoolean("doubleclick", false));
+
 	}
 
 	public void onClick(View v) {
@@ -47,39 +64,16 @@ public class Setting extends Activity {
 			finish();
 			break;
 		case R.id.stop:
-			stopService(new Intent(this, Serivces1.class));
-			stopService(new Intent(this, Serivces2.class));
+			stopService(new Intent(this, Serivces.class));
 			break;
 		}
 	}
 
-	private void loading() {
-		SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
-		String cmd = pref.getString("cmd", "");
-		String root = "" + pref.getBoolean("root", false);
-		String out = "" + pref.getBoolean("out", false);
-		String move = "" + pref.getBoolean("move", false);
-		String doubleclick = "" + pref.getBoolean("doubleclick", false);
-		text.setText("指令：" + cmd + "\n" + "Root：" + root + "\n" + "输出：" + out
-				+ "\n" + "移动模式" + move + "\n" + "双击模式" + doubleclick);
-		edit.setText(cmd);
-		box1.setChecked(pref.getBoolean("root", false));
-		box2.setChecked(pref.getBoolean("out", false));
-		box3.setChecked(pref.getBoolean("move", false));
-		mode = pref.getBoolean("move", false);
-		box4.setChecked(pref.getBoolean("doubleclick", false));
-
-	}
-
 	private void run() {
 
-		stopService(new Intent(this, Serivces1.class));
-		stopService(new Intent(this, Serivces2.class));
+		stopService(new Intent(this, Serivces.class));
+		startService(new Intent(this, Serivces.class));
 
-		if (mode) {
-			startService(new Intent(this, Serivces1.class));
-		} else {
-			startService(new Intent(this, Serivces2.class));
-		}
+//		}
 	}
 }
