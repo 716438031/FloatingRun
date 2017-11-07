2cf07e1769d359f8852d3556de25e551d82bda18
永远の萌新 <716438031@qq.com>
2017-11-07 11:13:39 +0800

    更新内容: 1,添加了注释(给我自己看的); 2,删除了多余的代码; 3,添加了记录悬浮窗位置的功能.
    待解决问题:
    代码还是不够精简;
    执行某些指令时提示拒绝访问(如ifconfig);
    待补充...

--- src/com/Floating/ShellRun/Serivces.java
+++ src/com/Floating/ShellRun/Serivces.java
@@ -1,8 +1,6 @@
 package com.Floating.ShellRun;
 
 import com.Floating.ShellRun.ShellUtils.CommandResult;
-import com.floatingview.shell.R;
-
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -11,18 +9,20 @@ import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.WindowManager.LayoutParams;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-public class Serivces2 extends Service {
+public class Serivces extends Service {
 
 	private boolean waitDouble = true;
-	private static final int DOUBLE_CLICK_TIME = 300;
+	private static final int DOUBLE_CLICK_TIME = 300;// 设定双击延迟
 
 	// 定义浮动窗口布局
 	LinearLayout mFloatLayout;
@@ -31,17 +31,24 @@ public class Serivces2 extends Service {
 	WindowManager mWindowManager;
 	Button mFloatView;
 	String string;
-	boolean root, out, doubleclick;
+	boolean root, out, doubleclick, move;
+	int x, y;
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		createFloatView();
 		SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+		// 从文件读取设定内容,不知道怎么传参好麻烦啊
 		string = pref.getString("cmd", "");
 		root = pref.getBoolean("root", false);
 		out = pref.getBoolean("out", false);
 		doubleclick = pref.getBoolean("doubleclick", false);
+		move = pref.getBoolean("move", false);
+		x = pref.getInt("x", 0);
+		y = pref.getInt("y", 0);
+
+		createFloatView();// 创建悬浮窗
+
 	}
 
 	@Override
@@ -49,7 +56,7 @@ public class Serivces2 extends Service {
 		return null;
 	}
 
-	private void createFloatView() {
+	private void createFloatView() {// 创建悬浮窗
 		wmParams = new WindowManager.LayoutParams();
 		getApplication();
 		// 获取WindowManagerImpl.CompatModeWrapper
@@ -62,33 +69,49 @@ public class Serivces2 extends Service {
 		// 设置浮动窗口不可聚焦（实现操作除浮动窗口外的其他可见窗口的操作）
 		wmParams.flags = LayoutParams.FLAG_NOT_FOCUSABLE;
 		// 调整悬浮窗显示的停靠位置为左侧置顶
-		wmParams.gravity = Gravity.LEFT;
+		wmParams.gravity = Gravity.LEFT | Gravity.TOP;
 
 		// 以屏幕左上角为原点，设置x、y初始值
-		wmParams.x = 0;
-		wmParams.y = 0;
+		wmParams.x = x;
+		wmParams.y = y;
 
 		// 设置悬浮窗口长宽数据
 		wmParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
 		wmParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
-
 		LayoutInflater inflater = LayoutInflater.from(getApplication());
 		// 获取浮动窗口视图所在布局
 		mFloatLayout = (LinearLayout) inflater.inflate(R.layout.serivces, null);
 		// 添加mFloatLayout
-		mWindowManager.addView(mFloatLayout, wmParams);
-
+		mWindowManager.addView(mFloatLayout, wmParams);// 设定悬浮窗位置
 		// 浮动窗口按钮
 		mFloatView = (Button) mFloatLayout.findViewById(R.id.float_id);
-
 		mFloatLayout.measure(View.MeasureSpec.makeMeasureSpec(0,
 				View.MeasureSpec.UNSPECIFIED), View.MeasureSpec
 				.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
 
+		// 设置监听浮动窗口的触摸移动
+		mFloatView.setOnTouchListener(new OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				// getRawX是触摸位置相对于屏幕的坐标，getX是相对于按钮的坐标
+				wmParams.x = (int) event.getRawX()
+						- mFloatView.getMeasuredWidth() / 2;
+				// 25为状态栏的高度
+				wmParams.y = (int) event.getRawY()
+						- mFloatView.getMeasuredHeight() / 2 - 25;
+				// 刷新
+				if (move) {
+					mWindowManager.updateViewLayout(mFloatLayout, wmParams);
+				}
+				return false;
+			}
+		});
+
+		// 点击事件
 		mFloatView.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				if (doubleclick) {
+				if (doubleclick) {// 判断双击执行,好复杂的逻辑,我已经看不懂了
 					if (waitDouble == true) {
 						waitDouble = false;
 						Thread thread = new Thread() {
@@ -120,28 +143,33 @@ public class Serivces2 extends Service {
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
+		// 保存悬浮窗位置
+		SharedPreferences.Editor editor = getSharedPreferences("data",
+				MODE_PRIVATE).edit();
+		editor.putInt("x", wmParams.x);
+		editor.putInt("y", wmParams.y);
+		editor.commit();
+
 		if (mFloatLayout != null) {
 			mWindowManager.removeView(mFloatLayout);
 		}
 	}
 
-	private void run() {
+	private void run() {// 执行命令
 		String[] commands = new String[] { string };
 		CommandResult result = ShellUtils.execCommand(commands, root);
-		if (out) {
-			if (!result.successMsg.equals("")) {
-				Toast.makeText(Serivces2.this, result.successMsg,
-						Toast.LENGTH_SHORT).show();
-				// 成功的提示
+		if (out) {// 是否输出执行返回内容
+			if (!result.successMsg.equals("")) {// 成功的提示
+				Toast.makeText(Serivces.this, result.successMsg, 0).show();
+
 			}
-			if (!result.errorMsg.equals("")) {
-				Toast.makeText(Serivces2.this, result.errorMsg,
-						Toast.LENGTH_SHORT).show();
-				// 错误的提示
+			if (!result.errorMsg.equals("")) {// 错误的提示
+				Toast.makeText(Serivces.this, result.errorMsg, 0).show();
+
 			}
 			if (result.successMsg.equals("") && result.errorMsg.equals("")) {
-				Toast.makeText(Serivces2.this, "没有返回值",
-						Toast.LENGTH_SHORT).show();
+				// 返回值为空的提示
+				Toast.makeText(Serivces.this, "没有返回值", 0).show();
 			}
 		}
 	}
